7cd51c8e171b3b3904a938c77d3ff6ea437859ef
Rohan Verma <rv725555@gmail.com>
2022-04-24 08:52:09 +0530

    improv: separated installation script for gnome-tweaks and script for modifying dock

--- dock.sh
+++ dock.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+#MacOS dock (changing the physical properties of the dock)
 
-#Adding Gnome Tweaks
-sudo add-apt-repository universe
-sudo apt install -y gnome-tweak-tool
-
-#MacOS dock
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
+
 #MacOS icons
+
 cd ~/Downloads
- git clone https://github.com/vinceliuice/McMojave-circle
+
+git clone https://github.com/vinceliuice/McMojave-circle
+
 cd ~/Downloads/McMojave-circle
+
 ./install.sh
---
+++ gnome-tweaks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#Adding Gnome Tweaks
+sudo add-apt-repository universe
+
+# For Ubuntu 20.04, the name was gnome-tweak-tool
+# For Ubuntu 22.04, the name is gnome-tweaks
+sudo apt install -y gnome-tweaks
