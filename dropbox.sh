52bc4447c026720eeaf0992e5440424151e64352
Rohan Verma <rv725555@gmail.com>
2022-04-24 08:52:36 +0530

    improv: simplified dropbox installation for Ubuntu 22.04 LTS

--- dropbox.sh
+++ dropbox.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-
-
 sudo apt upgrade
 
 #Adding GitHub to Ubuntu
 sudo apt install git
 
 #Adding Dropbox to Nautilus
-sudo apt install python3-gpg
-sudo apt install -y gnome-common libnautilus-extension-dev python3-gi python3-docutils
-cd ~/Downloads
-git clone https://github.com/dropbox/nautilus-dropbox.git
-cd ~/Downloads/nautilus-dropbox
-./autogen.sh
-make
-sudo make install
-./dropbox start -i
-killall nautilus
-sudo apt autoremove
 
+sudo apt install python3-gpg
+
+sudo apt install -y nautilus-dropbox
+
+killall nautilus
+
+sudo apt autoremove
