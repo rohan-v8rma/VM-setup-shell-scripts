eee4681111e365b5b5c679ad41cfc73fb1d527e1
Rohan Verma <rv725555@gmail.com>
2022-05-13 07:21:50 +0530

    improv: more compact syntax in if..elif..else..fi

--- 01s-test.sh
+++ 01s-test.sh
@@ -7,19 +7,18 @@
 x=1
 y=1
 
-if [[ $x -eq $y ]]
+if [[ $x -eq $y ]]; then 
 # spacing is important in this IF condition
 
-then 
-echo "$x equal to $y" 
+echo "$x equal to $y"
 # NOTE how the command substitution operator works within the echo command as well
 
-elif [[ $x -gt $y ]]
+elif [[ $x -gt $y ]]; then
 
-then
 echo "$x greater than $y" 
 
 else
+
 echo "$x lesser than $y"
 
 fi
