f936c8de494422c91e66154418315be8518ed627
Rohan Verma <rv725555@gmail.com>
2022-05-13 07:39:37 +0530

    improv: Added Help function

--- runall.sh
+++ runall.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-cd $(dirname $(readlink -f ./runall.sh))
 
-./test.sh
-./compilers-and-interpreters.sh
-./dropbox.sh
-./look-and-feel.sh
-./microsoft-edge.sh
-./vs-code.sh
-clear
+
+function Help() {
+    #########################################
+    #Help Function
+    #########################################
+    
+    echo "Hey! Welcome to Rohan's script for installing applications easily on linux."
+    echo "Whatever flag you add will help in specifying what application you want to install."
+    echo 
+    echo "Syntax: rohan-script [--<option>]"
+    echo
+    echo "Options:"
+    echo "--help                 This help text."
+    echo "--codeprocessors       A set of standard compilers and interpreters."
+    echo "--cpufreq              A software for managing CPU powersaving."
+    echo "--discord              A VoIP, instant messaging and digital distribution platform for gamers."
+    echo "--dockmodify           A script for changing the dock position to the bottom in GNOME."
+    echo "--dropbox              A script for integrating Dropbox with the Nautilus file manager."
+    echo "--gnometweaks          A script for install the GNOME tweaks software."
+    echo "--linwintime           A script for resolving the time discrepancy in a dual boot of Windows and Linux."
+    echo "--lxde                 A script for installing LXDE desktop environment."
+}
+   
+
+# ./test.sh
+# ./compilers-and-interpreters.sh
+# ./dropbox.sh
+# ./look-and-feel.sh
+# ./microsoft-edge.sh
+# ./vs-code.sh
+# clear
