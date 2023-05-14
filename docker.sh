5353c85b0a092e22121e0892a8f7a7b8d11931d1
Rohan Verma <rv725555@gmail.com>
2023-05-15 02:19:23 +0530

    improv: Simplified docker installation.

--- docker.sh
+++ docker.sh
@@ -3,27 +3,18 @@
 # Commands copied from https://docs.docker.com/engine/install/ubuntu/
 
 # 1. Uninstall old version
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt remove -y docker docker-engine docker.io containerd runc
 
-# 2. Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
+# 2. Install docker.io, docker ce and docker ce cli
 
-# 3. Add Docker’s official GPG key:
+# A.
+sudo apt install docker.io 
 
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# B. docker-ce-cli : command line interface for docker engine, COMMUNITY EDITION. Maintained by Docker.
+sudo apt install docker-ce-cli
 
-# 4. Use the following command to set up the repository:
+# C. docker-ce : docker engine, COMMUNITY EDITION. Requires docker-ce-cli. Maintained by Docker.
+sudo apt install docker-ce 
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# 5. Install Docker Engine, containerd, and Docker Compose.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# 6. Verify that the Docker Engine installation is successful by running the hello-world image.
+# 3. Verify that the Docker Engine installation is successful by running the hello-world image.
 sudo docker run hello-world
