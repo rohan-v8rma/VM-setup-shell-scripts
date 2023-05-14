5a9fa080b85993cfffd349bba36328d08e8692ff
Rohan Verma <rv725555@gmail.com>
2023-05-15 04:37:33 +0530

    fix: Changed file extension and standardized spacing.

--- cron-setup-copy-codes.sh
+++ cron-setup-copy-codes.sh
@@ -8,15 +8,15 @@ mkdir -p ~/.local/etc/cron.daily ~/.var/spool/anacron
 # --exclude='node_modules': The --exclude option specifies the pattern to exclude from the copy operation. In this case, we exclude any sub-folders named node_modules.
 # source_directory/: The path to the source directory you want to copy. Make sure to include the trailing slash / to indicate that it's a directory.
 # estination_directory/: The path to the destination directory where you want to copy the files. Again, include the trailing slash / to indicate it's a directory.
-echo "rsync -av --exclude='node_modules' ~/Dropbox/Shared/Computer_code/ ~/Documents/Computer_code/" > sync-codes.sh
+echo "rsync -av --exclude='node_modules' ~/Dropbox/Shared/Computer_code/ ~/Documents/Computer_code/" > sync-codes
 
-cp sync-codes.sh ~/.local/etc/cron.daily
+cp sync-codes ~/.local/etc/cron.daily
 
-chmod +x ~/.local/etc/cron.daily/sync-codes.sh
+chmod +x ~/.local/etc/cron.daily/sync-codes
 
 echo "SHELL=/bin/sh" > ~/.local/etc/anacrontab
 echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> ~/.local/etc/anacrontab
-echo "1  0  cron.sync-codes    run-parts ~/.local/etc/cron.daily/" >> ~/.local/etc/anacrontab
+echo "1 0 cron.sync-codes run-parts ~/.local/etc/cron.daily" >> ~/.local/etc/anacrontab
 
 # Performing a syntax check on anacrontab file.
 anacron -T -t ~/.local/etc/anacrontab -S ~/.var/spool/anacron
